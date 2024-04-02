3d36e8f86852c23d9f3c3f75b95970b6da99ffe8
CalliEtc <callipter@gmail.com>
2024-04-02 17:30:19 +0200

    structure globale du site

--- src/composants/listeRecettes/Liste.jsx
+++ src/composants/listeRecettes/Liste.jsx
@@ -9,12 +9,14 @@ function Liste ({recipe}) {
         {recipe && recipe.map(recipe => (
           <div key={recipe.id} className="container mx-auto shadow-md ">
             <img src={recipe.imageUrl} alt={recipe.title} style={{maxWidth: '100%'}} />
-            <h2 className="p-1 font-medium">{recipe.title}</h2>
+            <h2 className="p-1 font-medium text-green-700">{recipe.title}</h2>
+            <div className="p-1">
             <p><strong>Difficulté:</strong> {recipe.difficulty}</p>
             <p><strong>Catégorie:</strong> {recipe.category}</p>
             <p>{recipe.description}</p>
             <p><strong>Auteur:</strong> {recipe.author}</p>
-            <p className="text-right	">En savoir plus</p>
+            <p className="text-right text-green-700">En savoir plus</p>
+            </div>
           </div>
         ))}
         </div>
